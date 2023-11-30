b3175fdff01dfb448d0f9911170c4b8080c87eaa
Julie Foster <juliefoster6695@gmail.com>
2023-11-30 15:37:58 -0500

    create comments

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,2 @@
 from .category_tests import CategoryTests
+from .comment_tests import CommentTests
--- tests/comment_tests.py
+++ tests/comment_tests.py
@@ -7,7 +7,7 @@ from rareapi.models import Comment, RareUser
 
 
 class CommentTests(APITestCase):
-    fixtures = ["user", "rare_user", "tokens", "post", "comment", "category", "tag"]
+    fixtures = ["user", "rare_user", "token", "posts", "comments", "categories", "tags"]
 
     def setUp(self):
         self.user = User.objects.first()
@@ -15,33 +15,44 @@ class CommentTests(APITestCase):
         token = Token.objects.get(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {token.key}")
 
-    def test_change_comments(self):
-        comment = Comment()
-        comment.post_id = 1
-        comment.author_id = 1
-        comment.content = "Original Comment"
-        comment.save()
+    def test_get_all_comments(self):
+        response = self.client.get("/comments")
 
-        data = {"post": 1, "author": 1, "content": "Edited Comment"}
-
-        response = self.client.put(f"/comment/{comment.id}", data, format="json")
-
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-        response = self.client.get(f"/comment/{comment.id}")
         json_response = json.loads(response.content)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(json_response["content"], "Edited Comment")
 
-    def test_create_comment(self):
-        data = {"post": 1, "author": self.rare_user.id, "content": "test"}
+        self.assertEqual(json_response[0]["content"], "This is a great post!")
+        self.assertEqual(json_response[1]["content"], "I enjoyed reading this.")
+        self.assertEqual(json_response[2]["content"], "Interesting perspective.")
 
-        response = self.client.post("/comment", data, format="json")
+    # def test_change_comments(self):
+    #     comment = Comment()
+    #     comment.post = 1
+    #     comment.author = 1
+    #     comment.content = "Original Comment"
+    #     comment.save()
 
-        json_response = json.loads(response.content)
+    #     data = {"post": 1, "author": 1, "content": "Edited Comment"}
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(json_response["post"], 1)
-        self.assertEqual(json_response["author"], self.rare_user.id)
-        self.assertEqual(json_response["content"], "test")
+    #     response = self.client.put(f"/comments/{comment.id}", data, format="json")
+
+    #     self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    #     response = self.client.get(f"/comments/{comment.id}")
+    #     json_response = json.loads(response.content)
+
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #     self.assertEqual(json_response["content"], "Edited Comment")
+
+    # def test_create_comment(self):
+    #     data = {"post": 1, "author": self.rare_user.id, "content": "test"}
+
+    #     response = self.client.post("/comments", data, format="json")
+
+    #     json_response = json.loads(response.content)
+
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     self.assertEqual(json_response["post"], 1)
+    #     self.assertEqual(json_response["author"], self.rare_user.id)
+    #     self.assertEqual(json_response["content"], "test")
